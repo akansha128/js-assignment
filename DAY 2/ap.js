40f77cfd52abdc0267b226f6fc71a3ca7a7513ca
akansha128 <68181289+akansha128@users.noreply.github.com>
2020-07-16 13:26:09 +0530

    Add files via upload

---
+++ DAY 2/ap.js
@@ -0,0 +1,24 @@
+// que 1 : Get input from user and print it in a consoler
+
+console.log("hello");
+//let name =prompt("write your name here");
+//console.log(name);
+
+// que 2: methods of  string
+// substring
+let text = "letsupgrade";
+//console.log(text);
+console.log(text.length)
+console.log(text.substring(1,3));
+console.log(text.substring(2));
+
+//splice
+let num =['a','b','c','d'];
+num.splice(2, 3,'f');
+console.log(num);
+
+// replace
+let str ="hello guys"
+re = str.replace(/hello/,"hi");
+console.log(re);
+
