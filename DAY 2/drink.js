8a23b87ab36c6f921b06b3408f12dda8fdbd4414
akansha128 <68181289+akansha128@users.noreply.github.com>
2020-07-23 19:28:30 +0530

    Add files via upload

---
+++ DAY 2/drink.js
@@ -0,0 +1,10 @@
+console.log("...User...")
+let ans = prompt("Are you 21+ ?...yes or no")
+if (ans=="yes")
+{
+    console.log("You can drink")
+}
+if(ans=="no")
+{
+    console.log("You can't Drink")
+}
